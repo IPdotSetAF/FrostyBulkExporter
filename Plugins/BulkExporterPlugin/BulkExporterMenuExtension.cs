db6f43a61c898f36537e043fefee0e48bbc309b9
IPdotSetAF <ipdotsetaf@gmail.com>
2024-03-01 17:39:48 +0330

    added BulkExporterPlugin project added icon added menuItem toViews dropdown

---
+++ Plugins/BulkExporterPlugin/BulkExporterMenuExtension.cs
@@ -0,0 +1,21 @@
+﻿using Frosty.Core;
+using System.Windows.Media;
+
+namespace BulkExporterPlugin
+{
+    public class BulkExporterMenuExtension : MenuExtension
+    {
+        internal static ImageSource imageSource = new ImageSourceConverter().ConvertFromString("pack://application:,,,/BulkExporterPlugin;component/Images/BulkExporter.png") as ImageSource;
+
+        public override string TopLevelMenuName => "View";
+        public override string SubLevelMenuName => null;
+
+        public override string MenuItemName => "Bulk Exporter";
+        public override ImageSource Icon => imageSource;
+
+        public override RelayCommand MenuItemClicked => new RelayCommand((o) =>
+        {
+            App.EditorWindow.OpenEditor("Bulk Exporter", new BulkExporter(App.Logger));
+        });
+    }
+}
